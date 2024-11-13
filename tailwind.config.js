1b796b2f4fd942d784cd7d58953b6647510d9d6c
Julius Simonelli <julius.simonelli@gmail.com>
2024-11-13 08:10:50 -0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx,ts,tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/app/**/*.{js,jsx,ts,tsx}',
     './src/**/*.{js,jsx,ts,tsx}',
+    './index.html'
   ],
   prefix: "",
   theme: {
