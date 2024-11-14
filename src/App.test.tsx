4c2e9c8e142327cf259b72d4b3478dd473198554
Julius Simonelli <julius.simonelli@gmail.com>
2024-11-13 22:21:53 -0800

    rm test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test.skip('renders create election button', () => {
-    render(<App />);
-    const buttonElement = screen.getByText(/create new election/i);
-    expect(buttonElement).toBeInTheDocument();
-});
